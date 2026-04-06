300f18abd241d17b4740d659482ac6edd238d127
Brandon Wu <brwu1@uci.edu>
2026-04-05 19:26:55 -0700

    Create dataset for DistilBert model

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
+BERT_DIR = DATA_DIR / "bert"
 MODELS_DIR = BASE_DIR / "models"
